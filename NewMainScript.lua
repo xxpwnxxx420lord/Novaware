7184426ead3b54dcf6a10d37b2509e8445317bee
Luke <johnny.west9283@gmail.com>
2025-01-06 21:17:46 +1100

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -42,7 +42,7 @@ end
 
 if not shared.VapeDeveloper then
 	local _, subbed = pcall(function()
-		return game:HttpGet('https://github.com/7GrandDadPGN/VapeV4ForRoblox')
+		return game:HttpGet('https://github.com/xxpwnxxx420lord/Novaware')
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
